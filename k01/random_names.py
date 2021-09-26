b64e74e8e0e8c482692a4a83ab48b7034da0de04
Han Zhang <90658849+hzhangg20@users.noreply.github.com>
2021-09-26 09:09:52 -0400

    Add files via upload

---
+++ k01/random_names.py
@@ -0,0 +1,16 @@
+# Justin Zou, Mark Zhu, Han Zhang (Team MJH)
+# SoftDev
+# K01 -- Python Random Name Generator
+# 2021-09-22
+import random
+
+def printNames():
+    pd2 = ["Mark", "Justin", "Han", "Jacob", "Jonathan", "Raymond", "Kevin", "Annabel", "Andrew", "Thomas", "Noakai"]
+    pd1 = ["Angela", "Ella", "Ivan Lam", "Justin Morill", "Naomi", "Tina", "Tami" , "Christopher", "Yusuf", "Edwin", "Ishraq", "Emma", "Tomas"]
+    indexPD1 = random.randint(0,len(pd1)-1)
+    indexPD2 = random.randint(0,len(pd2)-1)
+    FiftyPercent = random.randint(0,1)
+    if (FiftyPercent == 0):
+        print("Pd1 name: " + pd1[indexPD1])
+    else:
+        print("Pd2 name: " + pd2[indexPD2])
