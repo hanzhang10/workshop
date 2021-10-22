55bfd098caffacff7df6319074ed1f766f87ea43
hzhangg20 <hanzhang99999@gmail.com>
2021-10-22 08:58:15 -0400

    removed useless line

--- 16_csv2db/db_builder.py
+++ 16_csv2db/db_builder.py
@@ -6,9 +6,6 @@
 import sqlite3   #enable control of an sqlite database
 import csv       #facilitate CSV I/O
 
-
-DB_FILE="discobandit.db"
-
 db = sqlite3.connect("Whales") #open if file exists, otherwise create
 c = db.cursor()               #facilitate db ops -- you will use cursor to trigger db events
 
@@ -19,18 +16,17 @@ c.execute("CREATE TABLE IF NOT EXISTS roster(name TEXT, age INTEGER, id INTEGER)
 with open("students.csv", mode = 'r') as students:  #reads in csv file
     file = csv.DictReader(students)
     for lines in file:                              #iterates through csv rows
-        script = "INSERT INTO roster VALUES(\"" + lines['name'] + "\"," + lines['age'] + "," + lines['id'] + ")" #scripts 
+        script = "INSERT INTO roster VALUES(\"" + lines['name'] + "\"," + lines['age'] + "," + lines['id'] + ")" #scripts
         c.execute(script)
 
 c.execute("CREATE TABLE IF NOT EXISTS courses(code TEXT, mark INTEGER, id INTEGER)") #creates table if one does not exist
 with open("courses.csv", mode = 'r') as courses:  #reads in csv file
     file = csv.DictReader(courses)
     for lines in file:                              #iterates through csv rows
-        script = "INSERT INTO courses VALUES(\"" + lines['code'] + "\"," + lines['mark'] + "," + lines['id'] + ")" #scripts 
+        script = "INSERT INTO courses VALUES(\"" + lines['code'] + "\"," + lines['mark'] + "," + lines['id'] + ")" #scripts
         c.execute(script)
 
 #==========================================================
 
 db.commit() #save changes
 db.close()  #close database
-
