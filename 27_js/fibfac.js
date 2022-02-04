dbdd806f535f64b3f3f513c30960a0d05231206b
hzhangg20 <hanzhang99999@gmail.com>
2022-02-04 09:37:23 -0500

    fixed js code

--- 27_js/fibfac.js
+++ 27_js/fibfac.js
@@ -1,15 +1,16 @@
 // Team HE:: Han Zhang, Edwin Zheng
 // SoftDev 
-//  K27 -- Basic functions in JavaScript
-//  2022-02-03
+// K27 -- Basic functions in JavaScript
+// 2022-02-03
 
 function fact(n) {
-	if (n > 1)
-		(n * (fact n - 1));
-	return n;
+	if (n <= 1)
+		return 1;
+	return (n * (fact (n - 1)));
 }
 
 function fibonacci(n) {
 	if (n <= 2)
 		return 1;
 	return fibonacci(n-1) + fibonacci(n-2);
+}
